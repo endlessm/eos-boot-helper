79c073e8d0362cc6104c22e63b6b7f316ee3a04e
Daniel Drake <drake@endlessm.com>
2014-03-10 14:13:57 -0600

    Initial commit

---
+++ dracut/repartition/module-setup.sh
@@ -0,0 +1,17 @@
+# Copyright (C) 2014 Endless Mobile, Inc.
+# Licensed under the GPLv2
+
+check() {
+  return 0
+}
+
+depends() {
+  echo fs-lib
+}
+
+install() {
+  dracut_install sfdisk
+  dracut_install basename
+  dracut_install readlink
+  inst_hook pre-mount 50 "$moddir/endless-repartition.sh"
+}
