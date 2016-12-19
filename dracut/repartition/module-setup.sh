5233c5efb4d79fe31dbb3af90d29906f83dd9ef5
João Paulo Rechi Vita <jprvita@endlessm.com>
2016-12-19 12:46:46 -0500

    reparition: Don't leave stale boot entries behind
    Changing the ESP's UUID breaks the Endless UEFI boot entry, since it
    uses it as part of the loader path. To avoid stale boot entries to
    accumulate on the machine, lets remove the Endless boot entry pointing
    to the original ESP's UUID.
    
    We could create a new boot entry here, but lets leave this job to
    fallback.efi, so we do not need to keep the bootloader's path in sync.
    
    https://phabricator.endlessm.com/T14430

--- dracut/repartition/module-setup.sh
+++ dracut/repartition/module-setup.sh
@@ -16,6 +16,9 @@ install() {
   dracut_install mkswap
   dracut_install sed
   dracut_install tune2fs
+  dracut_install efibootmgr
+  dracut_install grep
+  dracut_install cut
   dracut_install -o amlogic-fix-spl-checksum
   inst_script "$moddir/endless-repartition.sh" /bin/endless-repartition
   inst_simple "$moddir/endless-repartition.service" \
