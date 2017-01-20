7d5d482be6dceb271f78dc7bfe83ac8d7625b26f
Carlo Caione <carlo@endlessm.com>
2017-01-20 16:21:56 +0100

    image-boot: s/eos-live-etc-setup/eos-live-early-overlayfs-setup
    We are renaming eos-live-etc-setup to eos-live-early-overlayfs-setup to
    make it more generic.
    
    Signed-off-by: Carlo Caione <carlo@endlessm.com>

--- dracut/image-boot/module-setup.sh
+++ dracut/image-boot/module-setup.sh
@@ -16,10 +16,10 @@ install() {
   inst_script "$moddir"/eos-image-boot-setup /bin/eos-image-boot-setup
   inst_simple "$moddir"/eos-image-boot-setup.service \
 	"$systemdsystemunitdir"/eos-image-boot-setup.service
-  inst_simple "$moddir"/eos-live-etc-setup.service \
-	"$systemdsystemunitdir"/eos-live-etc-setup.service
+  inst_simple "$moddir"/eos-live-early-overlayfs-setup.service \
+	"$systemdsystemunitdir"/eos-live-early-overlayfs-setup.service
   mkdir -p "${initdir}${systemdsystemconfdir}/initrd-switch-root.target.wants"
-  ln_r "${systemdsystemunitdir}/eos-live-etc-setup.service" \
-        "${systemdsystemconfdir}/initrd-switch-root.target.wants/eos-live-etc-setup.service"
+  ln_r "${systemdsystemunitdir}/eos-live-early-overlayfs-setup.service" \
+        "${systemdsystemconfdir}/initrd-switch-root.target.wants/eos-live-early-overlayfs-setup.service"
   inst_script "$moddir/eos-image-boot-generator" $systemdutildir/system-generators/eos-image-boot-generator
 }
